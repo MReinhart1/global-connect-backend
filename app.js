5db46a53dde6c8a2eab26ea85d81282acb77115b
Michael Reinhart <michaelreinhart@macbook-michael.mynetworksettings.com>
2023-06-07 02:03:17 -0400

    add stuff for ec2

--- app.js
+++ app.js
@@ -6,12 +6,22 @@ var logger = require('morgan');
 require('dotenv').config()
 const mongoose = require('mongoose');
 const session = require('express-session')
+const fs = require("fs")
+
+
+var app = express();
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var policyRouter = require('./routes/Policies');
 
-var app = express();
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/Policies', policyRouter);
+
+
+
 
 
 // Mongoose
@@ -33,9 +43,10 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/Policies', policyRouter);
+if (!fs.existsSync("./uploads")){
+  fs.mkdirSync('./uploads');
+  fs.chmod( './uploads', "0o777" )
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -44,10 +55,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
